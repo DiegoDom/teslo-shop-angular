aaaab14d489bb97e212f59a05ae64d5a57c060a5
Diego Dominguez <diego_dominguez@fragua.com.mx>
2025-10-16 11:45:40 -0600

    local cache method

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { Product, ProductResponse } from '@products/interfaces/product.interface';
 import { environment } from 'src/environments/environment';
 
@@ -16,19 +16,38 @@ interface productsResponseParams {
 export class ProductsService {
   private http = inject(HttpClient);
 
+  private productsCache = new Map<string, ProductResponse>();
+  private singleProductsCache = new Map<string, Product>();
+
   getProducts(params: productsResponseParams): Observable<ProductResponse> {
     const { gender = '', limit = 9, offset = 0 } = params;
 
-    return this.http.get<ProductResponse>(`${baseURL}/products`, {
-      params: {
-        gender,
-        limit,
-        offset,
-      },
-    });
+    const key = `${limit}-${offset}-${gender}`;
+    if (this.productsCache.has(key)) {
+      return of(this.productsCache.get(key)!);
+    }
+
+    return this.http
+      .get<ProductResponse>(`${baseURL}/products`, {
+        params: {
+          gender,
+          limit,
+          offset,
+        },
+      })
+      .pipe(
+        tap((resp) => console.log(resp)),
+        tap((resp) => this.productsCache.set(key, resp))
+      );
   }
 
   getProductByIdSlug(idSlug: string): Observable<Product> {
-    return this.http.get<Product>(`${baseURL}/products/${idSlug}`);
+    if (this.singleProductsCache.has(idSlug)) {
+      return of(this.singleProductsCache.get(idSlug)!);
+    }
+    return this.http.get<Product>(`${baseURL}/products/${idSlug}`).pipe(
+      tap((product) => console.log(product)),
+      tap((product) => this.singleProductsCache.set(idSlug, product))
+    );
   }
 }
